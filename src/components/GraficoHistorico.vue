7d5b5640f4bb63f8b77dbd2eb829ee0c1ad62258
Joaco <joaquinamf@gmail.com>
2020-04-01 16:58:55 -0300

    Extrae comunicacion con la api a ServicioHistorico

--- src/components/GraficoHistorico.vue
+++ src/components/GraficoHistorico.vue
@@ -6,6 +6,7 @@
 
 <script>
 import LineChart from './LineChart.js'
+import ServicioHistorico from '@/services/ServicioHistorico.js'
 
 export default {
   name: 'GraficoHistorico',
@@ -15,85 +16,48 @@ export default {
   data () {
     return {
       datacollection: {},
-      diasDeLosCasos: [],
-      cantidadDeCasosPorDia: [],
-      cantidadDeMuertesPorDia: [],
-      cantidadDeRecuperadosPorDia: [],
-      diferenciaRecuperados: []
-
+      historico: null
     }
   },
   mounted () {
-     this.fillData()
-     this.axios
-      .get('https://corona.lmao.ninja/v2/historical/Argentina')
-      .then(historico => {
-
-        let lineaDeTiempo = historico.data.timeline;
-
-        let casosTotalesPorDia = lineaDeTiempo.cases;
-        let muertesTotalesPorDia = lineaDeTiempo.deaths;
-        let recuperadosTotalesPorDia = lineaDeTiempo.recovered;
-
-        this.diasDeLosCasos = Object.keys(casosTotalesPorDia).slice(43);
-        this.cantidadDeCasosPorDia = Object.values(casosTotalesPorDia).slice(43);
-        this.cantidadDeMuertesPorDia = Object.values(muertesTotalesPorDia).slice(43);
-        this.cantidadDeRecuperadosPorDia = Object.values(recuperadosTotalesPorDia).slice(43);
-        this.diferenciaRecuperados = this.cantidadDeCasosPorDia.map((valor, i) => {
-          return this.cantidadDeCasosPorDia[i] - this.cantidadDeMuertesPorDia[i] - this.cantidadDeRecuperadosPorDia[i];
-        })
-        this.exponencial = [];
-
-        for (let i = 0; i < this.diasDeLosCasos.length; i++) {
-          if(Number(this.diasDeLosCasos[i].substr(0,1)) > 2) {
-            this.exponencial.push(Math.exp(i));
-          }else{
-            this.exponencial.push(0);
-          }
-        }
-
-        this.muertesporDia = Object.values(casosTotalesPorDia);
-
-        this.fillData()
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
+    this.historico = ServicioHistorico;
+    this.historico.obtenerDatos().then(() => {
+      this.fillData()
+    });
   },
   methods: {
     fillData () {
+      console.log(this.historico.cantidadDeMuertesPorDia);
       this.datacollection = {
-        labels: this.diasDeLosCasos,
+        labels: this.historico.diasDeLosCasos,
         datasets: [
           {
             label: 'Muertes',
             backgroundColor: '#251515',
             borderColor: '#251515',
             fill: true,
-            data: this.cantidadDeMuertesPorDia
+            data: this.historico.cantidadDeMuertesPorDia
           },
           {
             label: 'Recuperados',
             backgroundColor: '#a7f578',
             borderColor: '#a7f578',
             fill: true,
-            data: this.cantidadDeRecuperadosPorDia
+            data: this.historico.cantidadDeRecuperadosPorDia
           },
           {
             label: 'Casos Activos',
             backgroundColor: '#cc7979',
             borderColor: '#cc7979',
             fill: true,
-            data: this.diferenciaRecuperados
+            data: this.historico.cantidadDeDiferenciaRecuperados
           },
           {
             label: 'Casos Totales',
             backgroundColor: '#f87979',
             borderColor: '#f87979',
             fill: true,
-            data: this.cantidadDeCasosPorDia
+            data: this.historico.cantidadDeCasosPorDia
           }
         ]
       }
---
+++ src/services/ServicioHistorico.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+export default {
+  diasDeLosCasos: [],
+  cantidadDeCasosPorDia: [],
+  cantidadDeMuertesPorDia: [],
+  cantidadDeRecuperadosPorDia: [],
+  cantidadDeDiferenciaRecuperados: [],
+
+  diaInicial: 43,
+
+  url: 'https://corona.lmao.ninja/v2/historical/',
+  pais: 'Argentina',
+
+  obtenerDatos () {
+    console.log(axios);
+    return axios.get(this.url + this.pais).then(historico => {
+      const lineaDeTiempo = historico.data.timeline
+
+      const casosTotalesPorDia = lineaDeTiempo.cases
+      const muertesTotalesPorDia = lineaDeTiempo.deaths
+      const recuperadosTotalesPorDia = lineaDeTiempo.recovered
+
+      this.diasDeLosCasos = Object.keys(casosTotalesPorDia).slice(this.diaInicial)
+      this.cantidadDeCasosPorDia = Object.values(casosTotalesPorDia).slice(this.diaInicial)
+      this.cantidadDeMuertesPorDia = Object.values(muertesTotalesPorDia).slice(this.diaInicial)
+      this.cantidadDeRecuperadosPorDia = Object.values(recuperadosTotalesPorDia).slice(this.diaInicial)
+
+      this.calcularDiferencial()
+      this.calcularExponencial()
+    })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+
+  calcularDiferencial () {
+    this.cantidadDeDiferenciaRecuperados = this.cantidadDeCasosPorDia.map((_valor, i) => {
+      return this.cantidadDeCasosPorDia[i] - this.cantidadDeMuertesPorDia[i] - this.cantidadDeRecuperadosPorDia[i]
+    })
+  },
+
+  calcularExponencial () {
+    this.exponencial = []
+
+    for (let i = 0; i < this.diasDeLosCasos.length; i++) {
+      if (Number(this.diasDeLosCasos[i].substr(0, 1)) > 2) {
+        this.exponencial.push(Math.exp(i))
+      } else {
+        this.exponencial.push(0)
+      }
+    }
+  }
+}
