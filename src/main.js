de00e404964a953a5839bfd94ccaa3f4021b6939
Joaco <joaquinamf@gmail.com>
2020-04-02 21:58:00 -0300

    Agrega bootstrap vue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 Vue.prototype.axios = axios
