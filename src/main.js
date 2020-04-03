31f126c0c85337221955be66577877423329bb98
Joaco <joaquinamf@gmail.com>
2020-04-02 22:35:55 -0300

    Importa bootstrap al proyecto y crea archivo de estilos principales

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './styles/principal.scss'
+
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
