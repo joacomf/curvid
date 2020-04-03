f6a6341bc6d3346fdb1e57c7c390e9e89fef5226
Joaco <joaquinamf@gmail.com>
2020-04-02 23:41:43 -0300

    Crea componente de menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <b-navbar type="light">
+      <div class="container">
+        <b-navbar-brand href="#">
+          <img src="@/assets/logo.svg" />
+          Curvid
+        </b-navbar-brand>
+      </div>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'app-menu'
+}
+</script>
